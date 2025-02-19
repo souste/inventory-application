07fb48bb2892cc0b1d5dbbd8d5266212e5b1bef6
Stephen <stephensouness@outlook.com>
2025-02-19 08:47:40 +0000

    creared a home page linking to the three main pages (and back), everything is not interconnected

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const genreRoutes = require("./routes/genreRoutes");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.render("index"));
 
 app.use("/games", gameRoutes);
 app.use("/developers", developerRoutes);
